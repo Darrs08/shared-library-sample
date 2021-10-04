07c17162ac6944282422069bde53797396f517cc
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-04 16:22:21 +0800

    Update step.groovy

--- vars/step.groovy
+++ vars/step.groovy
@@ -9,3 +9,9 @@ def buildNum(){
 def buildImage(user){
   myapp = docker.build("${user}/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
 }
+
+def pushImage(){
+  docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+  myapp.push("latest")
+  myapp.push("${env.BUILD_ID}")
+}
