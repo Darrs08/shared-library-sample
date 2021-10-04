a4b8ee8e07e0734f9d45bad081cface6ee30964b
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-04 16:10:27 +0800

    Update step.groovy

--- vars/step.groovy
+++ vars/step.groovy
@@ -4,5 +4,5 @@ def build(){
 
 def buildImage(){
   echo "Build image with tag: ${env.BUILD_ID}"
-  myapp = docker.build("darrs08/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
+  myapp = docker.build("darrs08/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=$'{env.BUILD_ID}' .")
 }
