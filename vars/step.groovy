6e83e72abd5786f1ddf1dc678529f9c21c1162cb
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-04 16:05:51 +0800

    Update step.groovy

--- vars/step.groovy
+++ vars/step.groovy
@@ -1,3 +1,8 @@
 def build(){
   sh 'mvn package'
 }
+
+def buildImage(){
+  echo "Build image with tag: ${env.BUILD_ID}"
+  myapp = docker.build("darrs08/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
+}
